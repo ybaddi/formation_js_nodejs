6afedb6d64d68cc26163c436d1b609b165a17f9d
Youssef baddi <baddiyoussef85@gmail.com>
2021-10-23 13:28:03 +0100

    add Ch1

---
+++ Ch1/script.js
@@ -0,0 +1,35 @@
+x=20;
+var y = 30;
+
+let z =0;
+
+// function
+function add(a, b){
+    return a+b;
+}
+
+let res = add(x,y);
+
+console.log(res);
+
+
+let a=20,b=30;
+
+function devide(a,b){
+    if(b == 0){
+        // console.log("error dividing by o");
+        throw 'erro dividing byt o';
+    }
+
+    return a / b;
+}
+
+
+let tab = [];
+
+let tab_1 =[1,2,3,'hello'];
+console.log(tab_1);
+
+console.log(tab_1[1]);
+
+console.log(tab_1.length);
